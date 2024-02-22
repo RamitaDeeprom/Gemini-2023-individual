f87e55657335b1d15b8a6303a23eec31f6494ac0
Ramita Deeprom <62588305+RamitaDeeprom@users.noreply.github.com>
2024-02-22 22:53:08 +0700

    Update Invoice.java

--- HolidayTravelVehicles/Invoice.java
+++ HolidayTravelVehicles/Invoice.java
@@ -1,6 +1,7 @@
 public class Invoice {
     private Customer CustomerInfo;
     private Vehicle TradeinVehicle;
+
     private boolean TradeinAllowance;
     private int FinalNegotiatePrice;
     private String PurchaseVegicleInfo;
@@ -19,73 +20,22 @@ public class Invoice {
         this.LicenseFees = licenseFees;
         this.InstallOption = installOption;
     }
-    public String getCustomerInfo() {
+    public Customer getCustomerInfo() {
         return CustomerInfo;
     }
 
-    public String getTradeinVehicle() {
+    public void setCustomerInfo(Customer customerInfo) {
+        CustomerInfo = customerInfo;
+    }
+
+    public Vehicle getTradeinVehicle() {
         return TradeinVehicle;
+    }
+
+    public void setTradeinVehicle(Vehicle tradeinVehicle) {
+        TradeinVehicle = tradeinVehicle;
     }
 
     public boolean isTradeinAllowance() {
         return TradeinAllowance;
     }
-
-    public void setTradeinAllowance(boolean tradeinAllowance) {
-        TradeinAllowance = tradeinAllowance;
-    }
-
-    public int getFinalNegotiatePrice() {
-        return FinalNegotiatePrice;
-    }
-
-    public void setFinalNegotiatePrice(int finalNegotiatePrice) {
-        FinalNegotiatePrice = finalNegotiatePrice;
-    }
-
-    public String getPurchaseVegicleInfo() {
-        return PurchaseVegicleInfo;
-    }
-
-    public void setPurchaseVegicleInfo(String purchaseVegicleInfo) {
-        PurchaseVegicleInfo = purchaseVegicleInfo;
-    }
-
-    public int getTaxes() {
-        return Taxes;
-    }
-
-    public void setTaxes(int taxes) {
-        Taxes = taxes;
-    }
-
-    public int getLicenseFees() {
-        return LicenseFees;
-    }
-
-    public void setLicenseFees(int licenseFees) {
-        LicenseFees = licenseFees;
-    }
-
-    public boolean isInstallOption() {
-        return InstallOption;
-    }
-
-    public void setInstallOption(boolean installOption) {
-        InstallOption = installOption;
-    }
-    public String getInvoiceInfo(){
-        String str = String.valueOf(this.CustomerInfo) +
-                this.TradeinVehicle +
-                this.TradeinAllowance +
-                this.FinalNegotiatePrice +
-                this.PurchaseVegicleInfo +
-                this.Taxes +
-                this.LicenseFees +
-                this.InstallOption;
-        return str;
-    }
-}
-
-
-
